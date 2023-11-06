9b862bf44a355143177c9b4943ff4e7dc365e73b
Naveen RS <rnaveen.tkt@gmail.com>
2023-11-06 11:59:07 +0530

    updated few shot examples

--- langchain_helper.py
+++ langchain_helper.py
@@ -36,7 +36,7 @@ def get_few_shot_db_chain():
         k=2,
     )
     mysql_prompt = """You are a Microsoft SQL Server expert. Given an input question, first create a syntactically correct Microsoft SQL Server query to run, then look at the results of the query and return the answer to the input question.
-    Unless the user specifies in the question a specific number of examples to obtain, query for at most {top_k} results using the LIMIT clause as per MySQL. You can order the results to return the most informative data in the database.
+    Unless the user specifies in the question a specific number of examples to obtain, query for at most {top_k} results using the TOP clause as per Microsoft SQL Server. You can order the results to return the most informative data in the database.
     Never query for all columns from a table. You must query only the columns that are needed to answer the question. Wrap each column name in backticks (`) to denote them as delimited identifiers.
     Pay attention to use only the column names you can see in the tables below. Be careful to not query for columns that do not exist. Also, pay attention to which column is in which table.
     Pay attention to use CURDATE() function to get the current date, if the question involves "today".
